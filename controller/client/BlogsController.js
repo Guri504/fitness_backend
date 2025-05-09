9b621e082ebe2c6db0bcef81ff5144731f32bf42
NAVI <ns5516517@gmail.com>
2025-05-09 13:30:33 +0530

    changes new

--- controller/client/BlogsController.js
+++ controller/client/BlogsController.js
@@ -8,6 +8,29 @@ const index = async (req, res) => {
     })
 }
 
-module.exports = { 
-    index
+const detail = async (req, res) => {
+    let { id } = req.params;
+    console.log(id)
+    let resp = await blogsModel.getById(id);
+    if (resp) {
+        res.send({
+            status: true,
+            message: "Blog find successfully",
+            data: resp
+        })
+    }
+    else {
+        res.send({
+            status: false,
+            message: "Not able to find blog",
+            data: []
+        })
+    }
+
+}
+
+
+module.exports = {
+    index,
+    detail
 }
--- routes/client/Blogs.js
+++ routes/client/Blogs.js
@@ -3,5 +3,6 @@ const clientBlogsRouter = express.Router();
 const clientBlogsController = require("../../controller/client/BlogsController");
 
 clientBlogsRouter.get('/client/blogs', clientBlogsController.index);
+clientBlogsRouter.get('/client/blog/view/:id', clientBlogsController.detail)
 
 module.exports = clientBlogsRouter;
